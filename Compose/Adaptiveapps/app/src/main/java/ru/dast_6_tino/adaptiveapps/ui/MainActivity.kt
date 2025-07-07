71fbb763e22b23b60ebb94894ab7f0b583b6f1f5
dast6tino-twin <dast6tino-twin@users.noreply.github.com>
2025-07-08 00:18:19 +0200

    Clean code

--- Compose/Adaptiveapps/app/src/main/java/ru/dast_6_tino/adaptiveapps/ui/MainActivity.kt
+++ Compose/Adaptiveapps/app/src/main/java/ru/dast_6_tino/adaptiveapps/ui/MainActivity.kt
@@ -6,13 +6,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Text
-import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import ru.dast_6_tino.adaptiveapps.data.local.LocalEmailsDataProvider
